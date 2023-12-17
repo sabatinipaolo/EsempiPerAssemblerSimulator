48774d7de9b14ffe702240b16d373a9ea6bb1794
sabatinipaolo <113015247+sabatinipaolo@users.noreply.github.com>
2023-12-17 18:09:54 +0100

    Update 130_prop_call.sol.asm

--- 130_prop_call.sol.asm
+++ 130_prop_call.sol.asm
@@ -1,1 +1,35 @@
+;completa tu il programma 
 
+; scrive 8 volte 255 in memoria a partire dalla   locazione di indirizzo 80 
+
+        mov d,80
+
+        mov c,8
+ciclo:  mov [d], 255
+        inc d
+        dec c
+        jnz ciclo
+
+
+; scrive 8 volte 255 in memoria a partire dalla   locazione di indirizzo 96 
+
+        mov d,96 
+
+         mov c,8
+ciclo1:  mov [d], 255
+         inc d
+         dec c
+         jnz ciclo1
+
+
+; scrive 8 volte 255 in memoria a partire dalla   locazione di indirizzo 112 
+
+        mov d,112
+
+        mov c,8
+ciclo2:  mov [d], 255
+         inc d
+         dec c
+         jnz ciclo2
+
+         hlt
