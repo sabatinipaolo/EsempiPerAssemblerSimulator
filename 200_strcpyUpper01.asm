f49efce4b885e5a140c28c094eac1eb76bb3fa4c
sabatinipaolo <113015247+sabatinipaolo@users.noreply.github.com>
2024-01-14 13:30:39 +0100

    Create 200_strcpyUpper01.asm

---
+++ 200_strcpyUpper01.asm
@@ -0,0 +1,34 @@
+; code segment
+
+    mov c, stringa_sorgente 
+    mov d, stringa_destinazione
+
+; quando capito il codice de-commenta la riga sotto 
+; e lancia il programma osservando cosa fa :
+    
+    mov d, 232
+
+while:
+    mov a, [c]
+    cmp a , 0 
+    je fine_while
+        
+        sub a , 32
+
+        mov [d], a
+        inc c
+        inc d 
+   
+    jmp while
+         
+fine_while:
+    mov [d],0
+
+    hlt
+
+; data segment
+
+    stringa_sorgente:     db "sono la stringa"
+                          db 0
+    stringa_destinazione: db "             "
+                          db 0
